fbec9e97402790c9d0a6cdd08a5d5f9d97d86ff0
PedroP <66853097+ordep2v@users.noreply.github.com>
2020-10-22 10:27:47 -0300

    Update auth.jsx

--- minierp-js-reactjs/frontend/src/auth/auth.jsx
+++ minierp-js-reactjs/frontend/src/auth/auth.jsx
@@ -30,7 +30,7 @@ class Auth extends Component {
         const { handleSubmit } = this.props
         return (
             <div className="login-box">
-                <div className="login-logo"><b> My</b> App</div>
+                <div className="login-logo"><b> Mini</b> ERP</div>
                 <div className="login-box-body">
                     <p className="login-box-msg">Bem vindo!</p>
                     <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
